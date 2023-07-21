b9488ad1d8f836ff3923f5c6905b513bcd9a1dd0
Empa <emanuel@empa.xyz>
2023-07-21 12:05:04 +0200

    The rest of the instructions

--- nes/src/cpu.rs
+++ nes/src/cpu.rs
@@ -165,8 +165,45 @@ impl Cpu {
         self.p &= !mask;
     }
 
-    fn update_negative(&mut self) {
-        self.set_flag(FLAG_NEGATIVE, (self.a as i8).is_negative());
+    fn update_negative(&mut self, v: u8) {
+        self.set_flag(FLAG_NEGATIVE, (v as i8).is_negative());
+    }
+
+    fn update_zero(&mut self, v: u8) {
+        self.set_flag(FLAG_ZERO, v == 0);
+    }
+
+    fn relative_jump(&mut self, d: i8) {
+        self.pc = self.pc.wrapping_add_signed(d as i16);
+    }
+
+    fn compare(&mut self, v: i8) {
+        self.set_flag(FLAG_CARRY, self.a as i8 >= v);
+        self.set_flag(FLAG_ZERO, self.a as i8 == v);
+        self.set_flag(FLAG_NEGATIVE, (self.a as i8).wrapping_sub(v).is_negative());
+    }
+
+    fn push8(&mut self, v: u8) {
+        self.write_memory(self.stack_ptr(), v);
+        self.s = self.s.wrapping_add(1);
+    }
+
+    fn pop8(&mut self) -> u8 {
+        let v = self.read_memory(self.stack_ptr());
+        self.s = self.s.wrapping_sub(1);
+        v
+    }
+
+    fn push16(&mut self, v: u16) {
+        let [l, r] = v.to_le_bytes();
+        self.push8(l);
+        self.push8(r);
+    }
+
+    fn pop16(&mut self) -> u16 {
+        let r = self.pop8();
+        let l = self.pop8();
+        u16::from_le_bytes([l, r])
     }
 
     fn op_adc(&mut self, v: u8) {
@@ -180,5 +217,290 @@ impl Cpu {
         self.a = a;
         self.set_flag(FLAG_CARRY, of1 || of2);
         self.set_flag(FLAG_OVERFLOW, was_positive && (self.a as i8).is_negative());
+    }
+
+    fn op_and(&mut self, v: u8) {
+        self.a &= v;
+        self.update_zero(self.a);
+        self.update_negative(self.a);
+    }
+
+    fn op_asl(&mut self, v: Option<u8>) {
+        let v = if let Some(x) = v { x } else { self.a };
+        self.a = v << 1;
+        self.set_flag(FLAG_CARRY, v & (1 << 7) != 0);
+        self.update_negative(self.a);
+    }
+
+    fn op_bcc(&mut self, d: i8) {
+        if !self.get_flag(FLAG_CARRY) {
+            self.relative_jump(d);
+        }
+    }
+
+    fn op_bcs(&mut self, d: i8) {
+        if self.get_flag(FLAG_CARRY) {
+            self.relative_jump(d);
+        }
+    }
+
+    fn op_beq(&mut self, d: i8) {
+        if self.get_flag(FLAG_ZERO) {
+            self.relative_jump(d);
+        }
+    }
+
+    fn op_bit(&mut self, m: u8) {
+        let v = self.a & m;
+        self.set_flag(FLAG_OVERFLOW, v & (1 << 6) != 0);
+        self.set_flag(FLAG_NEGATIVE, v & (1 << 7) != 0);
+        self.set_flag(FLAG_ZERO, v == 0);
+    }
+
+    fn op_bmi(&mut self, d: i8) {
+        if self.get_flag(FLAG_NEGATIVE) {
+            self.relative_jump(d);
+        }
+    }
+
+    fn op_bne(&mut self, d: i8) {
+        if !self.get_flag(FLAG_ZERO) {
+            self.relative_jump(d);
+        }
+    }
+
+    fn op_bpl(&mut self, d: i8) {
+        if !self.get_flag(FLAG_NEGATIVE) {
+            self.relative_jump(d);
+        }
+    }
+
+    fn op_brk(&mut self) {
+        // TODO, what should this do? Nothing?
+    }
+
+    fn op_bvc(&mut self, d: i8) {
+        if !self.get_flag(FLAG_OVERFLOW) {
+            self.relative_jump(d);
+        }
+    }
+
+    fn op_clc(&mut self) {
+        self.disable_flag(FLAG_CARRY);
+    }
+
+    fn op_cld(&mut self) {
+        self.disable_flag(FLAG_DECIMAL);
+    }
+
+    fn op_cli(&mut self) {
+        self.disable_flag(FLAG_INTERRUPT_DISABLE);
+    }
+
+    fn op_clv(&mut self) {
+        self.disable_flag(FLAG_OVERFLOW);
+    }
+
+    fn op_cmp(&mut self, m: i8) {
+        self.compare(m);
+    }
+
+    fn op_cpx(&mut self) {
+        self.compare(self.x as i8);
+    }
+
+    fn op_cpy(&mut self) {
+        self.compare(self.y as i8);
+    }
+
+    fn op_dec(&mut self, addr: u16) {
+        let v = self.read_memory(addr).wrapping_sub(1);
+        self.write_memory(addr, v);
+        self.update_zero(v);
+        self.update_negative(v);
+    }
+
+    fn op_dex(&mut self) {
+        let v = self.x.wrapping_sub(1);
+        self.x = v;
+        self.update_zero(v);
+        self.update_negative(v);
+    }
+
+    fn op_dey(&mut self) {
+        let v = self.y.wrapping_sub(1);
+        self.y = v;
+        self.update_zero(v);
+        self.update_negative(v);
+    }
+
+    fn op_eor(&mut self, m: u8) {
+        self.a ^= m;
+        self.update_zero(self.a);
+        self.update_negative(self.a);
+    }
+
+    fn op_inc(&mut self, addr: u16) {
+        let v = self.read_memory(addr).wrapping_add(1);
+        self.write_memory(addr, v);
+        self.update_zero(v);
+        self.update_negative(v);
+    }
+
+    fn op_inx(&mut self) {
+        let v = self.x.wrapping_add(1);
+        self.x = v;
+        self.update_zero(v);
+        self.update_negative(v);
+    }
+
+    fn op_iny(&mut self) {
+        let v = self.y.wrapping_add(1);
+        self.y = v;
+        self.update_zero(v);
+        self.update_negative(v);
+    }
+
+    fn op_jmp(&mut self, addr: u16) {
+        self.pc = addr;
+    }
+
+    fn op_jsr(&mut self, addr: u16) {
+        self.push16(self.pc - 1);
+        self.pc = addr;
+    }
+
+    fn op_lda(&mut self, m: u8) {
+        self.a = m;
+        self.update_zero(self.a);
+        self.update_negative(self.a);
+    }
+
+    fn op_ldx(&mut self) {
+        self.a = self.x;
+        self.update_zero(self.a);
+        self.update_negative(self.a);
+    }
+
+    fn op_ldy(&mut self) {
+        self.a = self.y;
+        self.update_zero(self.a);
+        self.update_negative(self.a);
+    }
+
+    fn op_lsr(&mut self, v: Option<u8>) {
+        let v = if let Some(x) = v { x } else { self.a };
+        self.a = v >> 1;
+        self.set_flag(FLAG_CARRY, v & (1 << 0) != 0);
+        self.update_negative(self.a);
+    }
+
+    fn op_nop(&mut self) {}
+
+    fn op_ora(&mut self, m: u8) {
+        self.a |= m;
+        self.update_zero(self.a);
+        self.update_negative(self.a);
+    }
+
+    fn op_pha(&mut self) {
+        self.push8(self.a);
+    }
+
+    fn op_php(&mut self) {
+        self.push8(self.p);
+    }
+
+    fn op_pla(&mut self) {
+        self.a = self.pop8();
+    }
+
+    fn op_plp(&mut self) {
+        self.p = self.pop8();
+    }
+
+    fn op_rol(&mut self, v: u8) -> u8 {
+        let new_v = v << 1 | if self.get_flag(FLAG_CARRY) { 1 } else { 0 };
+        self.set_flag(FLAG_CARRY, v & (1 << 7) != 0);
+        new_v
+    }
+
+    fn op_ror(&mut self, v: u8) -> u8 {
+        let new_v = v >> 1 | if self.get_flag(FLAG_CARRY) { 1 << 7 } else { 0 };
+        self.set_flag(FLAG_CARRY, v & 1 != 0);
+        new_v
+    }
+
+    fn op_rti(&mut self) {
+        self.p = self.pop8();
+        self.pc = self.pop16();
+    }
+
+    fn op_rts(&mut self) {
+        self.pc = self.pop16();
+    }
+
+    fn op_sbc(&mut self) {
+        todo!()
+    }
+
+    fn op_sec(&mut self) {
+        self.set_flag(FLAG_CARRY, true);
+    }
+
+    fn op_sed(&mut self) {
+        self.set_flag(FLAG_DECIMAL, true);
+    }
+
+    fn op_sei(&mut self) {
+        self.set_flag(FLAG_INTERRUPT_DISABLE, true);
+    }
+
+    fn op_sta(&mut self, addr: u16) {
+        self.write_memory(addr, self.a);
+    }
+
+    fn op_stx(&mut self, addr: u16) {
+        self.write_memory(addr, self.x);
+    }
+
+    fn op_sty(&mut self, addr: u16) {
+        self.write_memory(addr, self.y);
+    }
+
+    fn op_tax(&mut self) {
+        self.x = self.a;
+        self.update_zero(self.x);
+        self.update_negative(self.x);
+    }
+
+    fn op_tay(&mut self) {
+        self.y = self.a;
+        self.update_zero(self.y);
+        self.update_negative(self.y);
+    }
+
+    fn op_tsx(&mut self) {
+        self.x = self.s;
+        self.update_zero(self.x);
+        self.update_negative(self.x);
+    }
+
+    fn op_txa(&mut self) {
+        self.a = self.x;
+        self.update_zero(self.a);
+        self.update_negative(self.a);
+    }
+
+    fn op_txs(&mut self) {
+        self.a = self.x;
+        self.update_zero(self.a);
+        self.update_negative(self.a);
+    }
+
+    fn op_tya(&mut self) {
+        self.a = self.y;
+        self.update_zero(self.a);
+        self.update_negative(self.a);
     }
 }
