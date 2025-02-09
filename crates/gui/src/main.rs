1cd89c3279a631ea6ae376dce5e309b200d80b49
Emanuel Särnhammar <emanuel@quaqqer.com>
2025-02-09 22:43:23 +0100

    Set window title and icon

--- crates/gui/src/main.rs
+++ crates/gui/src/main.rs
@@ -9,8 +9,18 @@ use app::NemuApp;
 use eframe::egui::{self, Vec2};
 
 fn main() {
+    let icon = include_bytes!("../../../misc/logo.png");
+    let image = image::load_from_memory(icon).expect("Failed to load icon");
+
     let native_options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_min_inner_size(Vec2::new(512., 512.)),
+        viewport: egui::ViewportBuilder::default()
+            .with_title("Nemu")
+            .with_icon(egui::viewport::IconData {
+                rgba: image.to_rgba8().to_vec(),
+                width: image.width(),
+                height: image.height(),
+            })
+            .with_min_inner_size(Vec2::new(512., 512.)),
         ..Default::default()
     };
 
